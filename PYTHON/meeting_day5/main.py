80851217966ea0fd6858ec653937196da3235e3d
luhekapuspitawati <47882545+luhekapuspitawati@users.noreply.github.com>
2024-01-20 00:46:50 +0800

    day4

---
+++ PYTHON/meeting_day5/main.py
@@ -0,0 +1,14 @@
+# Learn List
+fruit = [1, 2]
+provinsi_of_indonesia = ["Aceh", "DKI Jakarta"]
+provinsi_of_indonesia[1] = "Bali"
+provinsi_of_indonesia.append("Papua")
+provinsi_of_indonesia.extend([" "])
+
+import random
+
+random.randint(1,10)
+
+
+
+print(provinsi_of_indonesia)
